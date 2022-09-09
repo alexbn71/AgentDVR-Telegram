c51b580a715f20dfcbbe14738e4e2173f729dd7f
alexbn71 <mrblason@gmail.com>
2022-09-09 10:13:39 +0200

    first commit

--- README.md
+++ README.md
@@ -6,14 +6,24 @@ It works with the free version of AgentDVR.
 
 ## PHP Configuration:
 
-Rename config.php_sample to config.php and change the content of the file.
+Rename config.php_sample to config.php and change the content of the file according to your configuration.
+
+Test your URL on browser: http://yourserverIP/telegram_alert.php?msg=TEST
+
+You should see a reply like this:
+
+```bash
+{"ok":true,"result":{"message_id":56,"from":{"id":1234567890,"is_bot":true,"first_name":"AgentDVR","username":"test_bot"},"chat":{"id":1234567890,"first_name":"My name","last_name":"My lastname","type":"private"},"date":1662710877,"text":"{MSG}"}}
+```
+
+When no image is passed to the script the notification is sent to Telegram only in text format.
 
 
 ## AgentDVR Configuration:
 
 ### Set these values to the new camera Action created
 
-Actions -> Action Configuration -> If = Manual Alert
+Actions -> Action Configuration -> If = Manual Alert (just for testing purposes)
 
 Actions -> Action Configuration -> Then = Call URL
 
@@ -30,6 +40,9 @@ Actions -> Action Configuration -> Method = POST
 2) Choose or type the /newbot command and send it.
 3) Choose a name for your bot — your subscribers will see it in the conversation. And choose a username for your bot — the bot can be found by its username in searches. The username must be unique and end with the word “bot.”
 4) After you choose a suitable name for your bot — the bot is created. You will receive a message with a link to your bot t.me/<bot_username>, recommendations to set up a profile picture, description, a list of commands to manage your new bot and the TOKEN.
+
+
+Now you can fire "Manual Alert" and see what happen on Telegram.
 
 Share any improvements to the script with me so that everyone can benefit!
 
--- telegram_alert.php
+++ telegram_alert.php
@@ -3,23 +3,30 @@
  * @author Alessandro Blason <mrblason@gmail.com>
  * @date 07/09/2022
  */
+// Report all PHP errors
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 include("config.php");
 
 // (AgentDVR) Actions -> Action Configuration -> Upload Image = 1
 $target_dir = "uploads/"; 
-$target_file = $target_dir . basename( $_FILES["file0"]["name"]);
-if (move_uploaded_file($_FILES["file0"]["tmp_name"], $target_file)) {
-    //echo "The file ". basename( $_FILES["file0"]["name"]). " has been uploaded.";
-} else {
-    //echo "Sorry, there was an error uploading your file.";
-}
+$target_file = "";
 
-// create curl file object from file
-if (function_exists('curl_file_create')) { 
-	$tmp_file = curl_file_create($target_file);
-} else { // 
-	$tmp_file = '@' . realpath($target_file);
+if (array_key_exists("file0", $_FILES)) {
+	$target_file = $target_dir . basename( $_FILES["file0"]["name"]);
+
+	if (move_uploaded_file($_FILES["file0"]["tmp_name"], $target_file)) {
+		//echo "The file ". basename( $_FILES["file0"]["name"]). " has been uploaded.";
+	} else {
+		//echo "Sorry, there was an error uploading your file.";
+	}
+	// create curl file object from file
+	if (function_exists('curl_file_create')) { 
+		$tmp_file = curl_file_create($target_file);
+	} else { // 
+		$tmp_file = '@' . realpath($target_file);
+	}
 }
 
 // (AgentDVR) Actions -> Action Configuration -> URL = http://yourserverIP/telegram_alert.php?msg={MSG}
@@ -28,14 +35,21 @@ if (isset($_GET["msg"])) {
 	$caption = $_GET["msg"];
 }	
 
-$data = array('chat_id' => $yourchatid, 'photo' => $tmp_file, 'caption' => $caption);
+if ($target_file != "") {
+	$data = array('chat_id' => $yourchatid, 'photo' => $tmp_file, 'caption' => $caption);
+	$function = "sendPhoto";
+} else {
+	$data = array('chat_id' => $yourchatid, 'text' => $caption);
+	$function = "sendMessage";
+}	
+
 $ch = curl_init();
 $headers = array("Content-Type:multipart/form-data");
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 // Telegram API url format: https://api.telegram.org/bot<THE TOKEN RECEIVED FROM BOTFATHER>/sendPhoto')
-curl_setopt($ch, CURLOPT_URL, 'https://api.telegram.org/bot' . $yourbottoken . '/sendPhoto');
+curl_setopt($ch, CURLOPT_URL, 'https://api.telegram.org/bot' . $yourbottoken . '/' . $function);
 curl_exec($ch);
 curl_close($ch);
 
