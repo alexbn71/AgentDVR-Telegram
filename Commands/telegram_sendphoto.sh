ba2b9aaa0817249780dcafb076f8c10d25e12430
alexbn71 <mrblason@gmail.com>
2022-09-12 07:27:33 +0200

    first commit

--- Commands/telegram_sendphoto.sh
+++ Commands/telegram_sendphoto.sh
@@ -14,7 +14,7 @@ APIPWD=AgentDVR API password
 
 ID=$1
 MSG=$2
-curl -v -u ${APIUSER}:${APIPWD} "http://${AGENTDVRURL}:8090/grab.jpg?oid=${ID}&size=320x240" > /agent/Commands/grabs/grab${ID}.jpg
+curl -v -u ${APIUSER}:${APIPWD} "http://${AGENTDVRURL}:8090/grab.jpg?oid=${ID}&size=1280x720&maintainAR=true" > /agent/Commands/grabs/grab${ID}.jpg
 curl -v -F chat_id=${CHATID} \
 -F photo=@/agent/Commands/grabs/grab${ID}.jpg \
 -F caption="${MSG}" \
