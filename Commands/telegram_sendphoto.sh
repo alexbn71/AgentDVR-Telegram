4956ce13521d752313080f8be3b3b33140c34e78
Alessandro Blason <mrblason@gmail.com>
2023-09-25 07:37:01 +0200

    Update telegram_sendphoto.sh

--- Commands/telegram_sendphoto.sh
+++ Commands/telegram_sendphoto.sh
@@ -5,21 +5,13 @@
 #################################################
 
 ## UPDATE THESE VARIABLES 
-##AGENTDVRURL=your server IP or address
 CHATID=your telegram id
 TOKEN=your bot token
-##APIUSER=AgentDVR API username
-##APIPWD=AgentDVR API password
 #################################################
 
 ID=$1
 MSG=$2
 BASE64IMAGE=$3
-##curl -v -u ${APIUSER}:${APIPWD} "http://${AGENTDVRURL}:8090/grab.jpg?oid=${ID}&size=1280x720&maintainAR=true" > /agent/Commands/grabs/grab${ID}.jpg
-##curl -v -F chat_id=${CHATID} \
-##-F photo=@/agent/Commands/grabs/grab${ID}.jpg \
-##-F caption="${MSG}" \
-##https://api.telegram.org/bot${TOKEN}/sendPhoto
 echo ${BASE64IMAGE} > /AgentDVR/Commands/grabs/grab64_${ID}.jpg
 base64 --decode /AgentDVR/Commands/grabs/grab64_${ID}.jpg > /AgentDVR/Commands/grabs/grab64d_${ID}.jpg
 curl -v -F chat_id=${CHATID} \
